f51425c17382630c156d5fe8dcf6776be0715048
yutaro76 <53668660+yutaro76@users.noreply.github.com>
2021-05-10 18:29:51 +0900

    便利な関数（日付・時間）
    php

---
+++ 2-15/index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo time();
+echo "<br>";
+var_dump(time());
+echo "<br>";
+echo date("Y-m-d H:i:s", time());
+echo "<br>";
+echo date("Y-m-d H:i:s", 1620637945);
+echo "<br>";
+echo strtotime("2017/2/16 11:25:00");
+echo "<br>";
+echo "ログ(意味、確認の仕方)...処理の経過の表示・display_errorsなどを使う";
+echo "<br>";
+echo "クラウドサーバー、レンタルサーバー、VPSの違い...自由度・・・クラウド＞VPS＞レンタル"
+echo "<br>";
+echo "PHPのFWの種類と特徴...CakePHP（有名）, Laravel（きれい）など"
+echo "<br>";
+echo "<br>";
+
+?>
